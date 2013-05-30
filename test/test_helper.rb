5a93ace7ec22401ad8fdd4544cadda4ad7506b38
jgmodgoh <jgmodgoh@gmail.com>
2013-05-30 08:14:53 -0400

    Update test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,10 +8,10 @@ require 'vcr'
 
 def userConfig
 	{
-	:apiKey => 'm36b3xn5zxq6',
-	:apiSecret => 'OYjNv31lsEHMA6k5',
-	:userToken => 'b41df4e2-148c-46a0-a49a-1d81aa075b1f',
-	:userSecret => '8b4ff6cc-5bdc-4403-893c-0819f9e9186a',
+	:apiKey => 'XXXXXXXXX',
+	:apiSecret => 'xxxxxxXXXXxxxx',
+	:userToken => 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXX',
+	:userSecret => 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXX',
 	:configuration => {site: 'https:://api.linkedin.com'}
 	}
 end
