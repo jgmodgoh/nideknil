3001290a569c6283a5c0f254d99fc89967a653f0
jgmodgoh <jgmodgoh@gmail.com>
2013-05-30 15:16:11 -0300

    Update test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,10 +8,10 @@ require 'vcr'
 
 
 def testuser_config
-  { api_key:       'm36b3xn5zxq6',
-    api_secret:    'OYjNv31lsEHMA6k5',
-    user_token:    'b41df4e2-148c-46a0-a49a-1d81aa075b1f',
-    user_secret:   '8b4ff6cc-5bdc-4403-893c-0819f9e9186a',
+  { api_key:       'XXXxxXXXXX',
+    api_secret:    'XXXXXXXXXX',
+    user_token:    'XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXX',
+    user_secret:   'XXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXX',
     configuration: {site: 'https:://api.linkedin.com'}
   }
 end
