80cfe7c2e9180ee4296e88dc3ea4b9b08a6bf5ad
John Goh <jgmod@hotmail.com>
2013-05-31 12:29:43 -0400

    updated config method

--- lib/nideknil/client.rb
+++ lib/nideknil/client.rb
@@ -1,60 +1,64 @@
-require 'ostruct'
 
 module Nideknil  
   class Client
 
-    attr_reader :access_token
+    attr_reader :access_token, :secrets
 
     # Public: Initialize a Nideknil Client.
     #
-    # user_config - The Hash containing user key, secrets and token. The Hash needs to be of
+    # user_secrets - The Hash containing user key, secrets and token. The Hash needs to be of
     #               
-    #  user_config = {api_key:     "api key",
-    #                api_secret:  "api secret",
-    #                user_token:  "user token",
-    #                user_secret: "user secret"} 
+    #  user_secrets = {api_key:     "api key",
+    #                 api_secret:  "api secret",
+    #                 user_token:  "user token",
+    #                 user_secret: "user secret"} 
     #               
     #                where the values are unique inputs for each user from LinkedIn.
     #
     # Examples
     #
-    #   user_config = {api_key:     "123456789012",
-    #                api_secret:  "1234567890123456",
-    #                user_token:  "1234567-1234-1234-1234-123456789012",
-    #                user_secret: "1234567-1234-1234-1234-123456789012"}   
+    #   user_secrets = {api_key:     "123456789012",
+    #                  api_secret:  "1234567890123456",
+    #                  user_token:  "1234567-1234-1234-1234-123456789012",
+    #                  user_secret: "1234567-1234-1234-1234-123456789012"}   
     #
-    #   Nideknil::Client.new(user_config)
-    #   # => #<OAuth::AccessToken:0x007fa3d3c28818 @token="XXXX", @secret="XXXX", @consumer="XXXX", @params={XXXX}>
+    #   Nideknil::Client.new(user_secrets)
+    #   # => #<OAuth::AccessToken:0x007fa3d3ce6b88 @token="user token", @secret="user secret", 
+    #        @consumer=#<OAuth::Consumer:0x007fa3d3af2ef8 @key="api key", @secret="api secret", 
+    #        @options={:signature_method=>"HMAC-SHA1", :request_token_path=>"/oauth/request_token", 
+    #        :authorize_path=>"/oauth/authorize", :access_token_path=>"/oauth/access_token", 
+    #        :proxy=>nil, :scheme=>:header, :http_method=>:post, :oauth_version=>"1.0", 
+    #        :site=>"https://api.linkedin.com"}>, @params={}>
     #
     # Returns an OAuth::AccessToken with user configuration or default configuration if no arg is provided
-    def initialize(user_config={})
-      raise ArgumentError, 'argument needs to be hash' unless user_config.instance_of? Hash
-      @user_config = config(user_config)
+    def initialize(user_secrets={})
+      raise ArgumentError, 'argument needs to be hash' unless user_secrets.instance_of? Hash
+      config(user_secrets)
       @access_token = get_access_token
     end
 
     def client
-      OAuth::Consumer.new(user_config[:api_key], user_config[:api_secret], user_config[:options])
+      OAuth::Consumer.new(secrets[:api_key], secrets[:api_secret], secrets[:options])
     end
 
     def get_access_token
-      OAuth::AccessToken.new(client, user_config[:user_token], user_config[:user_secret])
+      OAuth::AccessToken.new(client, secrets[:user_token], secrets[:user_secret])
     end
 
-    def config(user_config)
-      prepared_config = 
-      {	api_key:     "api key",
-        api_secret:  "api secret",
-        user_token:  "user token",
-        user_secret: "user secret",
-        options:     {site: "https://api.linkedin.com"}
-        }.merge(user_config)
-        prepared_config
-      end
+    def config(user_secrets)
+      @secrets = @secrets ? @secrets.merge(user_secrets) : default_config.merge(user_secrets); 
+      self
+    end
 
-      def user_config
-        @user_config 
-      end
+    def default_config
+      {
+       api_key:     "api key",
+       api_secret:  "api secret",
+       user_token:  "user token",
+       user_secret: "user secret",
+       options:     {site: "https://api.linkedin.com"}
+      }      
+    end
 
   end
 end
--- test/spec_oauth.rb
+++ test/spec_oauth.rb
@@ -2,13 +2,20 @@ require 'test_helper'
 
 describe "Nideknil::Client" do
 
-  let(:site) {testuser_config[:configuration]}
-  let(:user) {Nideknil::Client.new}
-  let(:configured_user) {Nideknil::Client.new(testuser_config)}
+  let(:site) {test_config[:options]}
+  let(:new_user) {Nideknil::Client.new}
+  let(:configured_user) {Nideknil::Client.new(test_config)}
 
   describe "instantiating new object" do
-    it "with configured tokens & keys should initialise valid accessToken variable" do
+    it "with no inputs should initialize default access token variable" do
+      new_user.access_token.must_be_instance_of OAuth::AccessToken
+      new_user.access_token.secret.must_equal 'user secret'
+      new_user.access_token.consumer.site.must_equal 'https://api.linkedin.com'
+    end
+
+    it "with configured tokens & keys should initialize configured access token variable" do
       configured_user.access_token.must_be_instance_of OAuth::AccessToken
+      configured_user.access_token.secret.must_equal test_config[:user_secret]
     end
 
     it "without hash should raise error" do
@@ -17,15 +24,29 @@ describe "Nideknil::Client" do
   end
 
   describe "#config" do
-    it "should return default config hash" do
-      default_config = user.config(testuser_config)
-      default_config[:options][:site].must_equal 'https://api.linkedin.com'
+    it "should return default configured user when passed empty hash argument" do
+      test_user = new_user.config(empty_hash={})
+      test_user.secrets[:user_secret].must_equal "user secret"
+    end
+
+    describe "update user config" do
+      let(:test_user){new_user.config({user_secret: "updated user secret"})}
+      
+      it "should update user secret" do
+        test_user.secrets[:user_secret].must_equal "updated user secret"
+      end
+
+      it "should update user secret while keeping previously updated secrets" do
+        test_user.config({api_secret: "updated api secret"})
+        test_user.secrets[:api_secret].must_equal "updated api secret"
+        test_user.secrets[:user_secret].must_equal "updated user secret"
+      end
     end
   end
 
   describe "#client" do
     it "should return a valid OAuth::Consumer object configured for LinkedIn API" do
-      oauthInstance = user.client
+      oauthInstance = new_user.client
       oauthInstance.must_be_instance_of OAuth::Consumer
       oauthInstance.site.must_equal 'https://api.linkedin.com'
     end
@@ -40,8 +61,8 @@ describe "Nideknil::Client" do
       end
     end
 
-    it "with invalid keys & tokens should return unauthorized from LinkedIn" do
-      invalid_token = user.get_access_token
+    it "with no keys & tokens should return unauthorized from LinkedIn" do
+      invalid_token = new_user.get_access_token
       VCR.use_cassette('invalid_access_token') do
         invalid_token.get("http://api.linkedin.com/v1/people/~").inspect.must_match "Net::HTTPUnauthorized 401"
       end		
